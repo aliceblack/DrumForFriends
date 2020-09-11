afd1fb58e09bbd0146f358f96da43364314ec8f5
Alice Sasso <alice.sasso@wintech.it>
2020-09-11 16:56:54 +0200

    Bootstrap4 and modal

---
+++ index.js
@@ -0,0 +1,12 @@
+var horseAudio = document.getElementById("horseAudio"); 
+
+function playAudio() { 
+    horseAudio.play(); 
+} 
+
+function pauseAudio() { 
+    horseAudio.pause(); 
+} 
+
+
+$('#instructions').modal('show')
